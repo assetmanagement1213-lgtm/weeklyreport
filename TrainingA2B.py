0828ae01dcf1848f242380ab06736b34da5c3fe5
assetmanagement1213-lgtm <assetmanagement1213@gmail.com>
2026-02-20 10:52:42 +0900

    Update TrainingA2B.py

--- TrainingA2B.py
+++ TrainingA2B.py
@@ -373,7 +373,35 @@ def app():
         """, unsafe_allow_html=True)
 
     st.divider()
+    st.markdown("""
+        <style>
+        .square-img {
+            width: 100%;
+            aspect-ratio: 1 / 1;
+            overflow: hidden;
+            border-radius: 14px;
+            box-shadow: 0 4px 12px rgba(0,0,0,0.08);
+        }
 
+        .square-img img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            transition: 0.3s ease-in-out;
+        }
+
+        .square-img img:hover {
+            transform: scale(1.05);
+        }
+
+        .img-caption {
+            text-align: center;
+            font-size: 14px;
+            margin-top: 6px;
+            color: #444;
+        }
+        </style>
+        """, unsafe_allow_html=True)
     st.markdown("""
             <style>
                 .header-subactivity h1 {
@@ -387,7 +415,8 @@ def app():
                 <h1>📸 Dokumentasi Kegiatan</h1>
             </div>""",unsafe_allow_html=True)
     import requests
-    import streamlit as st
+    import base64
+
 
     cols_per_row = 3
     rows = training_dokumentasi.to_dict("records")
@@ -407,10 +436,15 @@ def app():
                     with col:
                         # Jika benar-benar image
                         if "image" in content_type:
-                            st.image(
-                                response.content,
-                                caption=row.get("Keterangan", "")
-                            )
+                            img_base64 = base64.b64encode(response.content).decode()
+                            st.markdown(f"""
+                            <div class="square-img">
+                                <img src="data:image/jpeg;base64,{img_base64}">
+                            </div>
+                            <div class="img-caption">
+                                {row.get("Keterangan","")}
+                            </div>
+                            """, unsafe_allow_html=True)
                         else:
                             st.warning("Link tidak mengembalikan file gambar.")
                             st.write(row["url_clean"])
@@ -418,6 +452,7 @@ def app():
                 except Exception as e:
                     with col:
                         st.error(f"Error load: {e}")
+
 
 
 
