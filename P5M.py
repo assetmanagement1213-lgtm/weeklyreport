64d5b8494c27b473d1b52bfc01ef79b21036b595
assetmanagement1213-lgtm <assetmanagement1213@gmail.com>
2026-02-27 09:11:09 +0900

    Update P5M.py

--- P5M.py
+++ P5M.py
@@ -56,7 +56,7 @@ def app():
     week1_start = date(2026, 1, 2)
     today = date.today()
     days_since = (today - week1_start).days
-    current_week = (days_since // 7) + 1
+    current_week = (days_since // 7)
     default_week = f"Week {current_week}"
     default_week = [default_week] if default_week in weeks else []
 
@@ -176,4 +176,5 @@ def app():
                 except Exception as e:
 
                     st.error(f"Gagal load gambar: {e}")
+
 
