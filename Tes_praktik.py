d060bfc60b82e50b28e224ac12a5c36db0b20379
assetmanagement1213-lgtm <assetmanagement1213@gmail.com>
2026-02-27 09:14:37 +0900

    Update Tes_praktik.py

--- Tes_praktik.py
+++ Tes_praktik.py
@@ -59,7 +59,7 @@ def app():
     week1_start = date(2026, 1, 2)
     today = date.today()
     days_since = (today - week1_start).days
-    current_week = (days_since // 7) + 1
+    current_week = (days_since // 7)
     default_week = f"Week {current_week}"
     default_week = [default_week] if default_week in weeks else []
 
@@ -804,5 +804,6 @@ def app():
                 except Exception as e:
                     with col:
                         st.error(f"Error load: {e}")
+
 
 
