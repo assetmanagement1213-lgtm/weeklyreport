48070cedd172b0d7a3bd0147f95e5eb424388970
assetmanagement1213-lgtm <assetmanagement1213@gmail.com>
2026-03-06 07:55:29 +0900

    Update Inspeksi.py

--- Inspeksi.py
+++ Inspeksi.py
@@ -77,10 +77,10 @@ def app():
     # ================= WEEK FILTER ==================
     weeks = sorted(df_observasi["Week"].dropna().unique())
 
-    week1_start = date(2026, 1, 2)
+    week1_start = date(2026, 1, 1)
     today = date.today()
     days_since = (today - week1_start).days
-    current_week = (days_since // 7)+1
+    current_week = (days_since // 7)
     default_week = f"Week {current_week}"
     default_week = [default_week] if default_week in weeks else []
 
@@ -800,6 +800,7 @@ def app():
                     with col:
 
                         st.error(f"Error load: {e}")
+
 
 
 
